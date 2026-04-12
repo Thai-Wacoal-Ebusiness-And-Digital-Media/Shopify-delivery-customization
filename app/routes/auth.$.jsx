06cded748c377f801d753fdb4d1b0f144cc90efb
Chatree Benjasinpradit <pingsovo@gmail.com>
2026-04-12 10:04:37 +0700

    fix: update authentication strategy to use admin instead of callback in auth route

--- app/routes/auth.$.jsx
+++ app/routes/auth.$.jsx
@@ -2,6 +2,6 @@ import { authenticate } from "../shopify.server";
 
 // Handles all /auth/* routes: login, callback, token exchange
 export async function loader({ request }) {
-  await authenticate.callback(request);
+  await authenticate.admin(request);
   return null;
 }
